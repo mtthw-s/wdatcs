5d185664b7537238b75382fe3618df4baf868601
Ubuntu <ubuntu@ip-172-31-46-97.us-west-2.compute.internal>
2019-06-03 23:35:53 +0000

    Adding additional formatting to footer and links to the header

--- scripts/main.js
+++ scripts/main.js
@@ -9,6 +9,16 @@
         self.hideElement = function(id){
             document.getElementById(id).style.display = 'none';
         }
+        
+        self.createNewReview = function(e){
+            var clone = document.getElementsByClassName("review-div")[0].cloneNode(true);
+            clone.getElementsByTagName("p")[0].innerHTML = e.userName;
+            clone.getElementsByTagName("p")[1].innerHTML = e.review;
+            clone.getElementsByTagName("h4")[0].innerHTML = e.title;
+            document.getElementById("reviewList").prepend(clone);
+            return null;
+            
+        }
     }
     window.app = new App();
     
